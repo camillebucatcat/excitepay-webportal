bbc967e57e57e134f28dc2631328977597d0e969
jusjasjaz <jazminelean.dimagiba.xentra@gmail.com>
2023-06-15 02:44:45 +0800

    add prepaid component

---
+++ src/app/shared/prepaid/prepaid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prepaid',
+  templateUrl: './prepaid.component.html',
+  styleUrls: ['./prepaid.component.scss']
+})
+export class PrepaidComponent {
+
+}
