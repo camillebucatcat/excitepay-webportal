6b55155fa4098eaa6e4ebaa742ab980dd6340119
Camille Bucatcat <camille.bucatcat@gmail.com>
2023-06-17 00:39:17 +0800

    update featured card

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,19 @@ module.exports = {
           700: 'var(--excite-pay-primary-700)',
           800: 'var(--excite-pay-primary-800)',
           900: 'var(--excite-pay-primary-900)'
+        },
+        dark: {
+          DEFAULT: 'var(--excite-pay-dark)',
+          50: 'var(--excite-pay-dark-50)',
+          100: 'var(--excite-pay-dark-100)',
+          200: 'var(--excite-pay-dark-200)',
+          300: 'var(--excite-pay-dark-300)',
+          400: 'var(--excite-pay-dark-400)',
+          500: 'var(--excite-pay-dark-500)',
+          600: 'var(--excite-pay-dark-600)',
+          700: 'var(--excite-pay-dark-700)',
+          800: 'var(--excite-pay-dark-800)',
+          900: 'var(--excite-pay-dark-900)'
         }
       }
     },
