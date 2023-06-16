a8a7e880abaf6ed8399ecb5c012d8ac21ef872fb
amiranm <amira.nagahmahrous.xentra@gmail.com>
2023-06-17 01:56:24 +0800

    updated products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzSelectComponent, NzSelectModule } from 'ng-zorro-antd/select';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzRateModule } from 'ng-zorro-antd/rate';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +34,13 @@ registerLocaleData(en);
     BrowserAnimationsModule,
     NzButtonModule,
     NzDropDownModule,
-    NzSelectModule
+    NzSelectModule,
+    NzInputModule,
+    NzGridModule,
+    NzRateModule,
+    NzCollapseModule,
+    NzIconModule,
+    NzInputNumberModule
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US }
--- src/app/pages/product/product.module.ts
+++ src/app/pages/product/product.module.ts
@@ -3,15 +3,37 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
-
+import { GiftcardsComponent } from 'src/app/components/giftcards/giftcards.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzRateModule } from 'ng-zorro-antd/rate';
+import { FormsModule } from '@angular/forms';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
 
 @NgModule({
   declarations: [
-    ProductComponent
+    ProductComponent,
+    GiftcardsComponent
   ],
   imports: [
     CommonModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    NzButtonModule,
+    NzDropDownModule,
+    NzSelectModule,
+    NzInputModule,
+    NzGridModule,
+    NzRateModule,
+    FormsModule,
+    NzCollapseModule,
+    NzIconModule,
+    NzInputNumberModule
+    
   ]
 })
 export class ProductModule { }
